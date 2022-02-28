ae1ff4670b37a034508019a1dabb894dc210300a
Toyalp <lapitts@redventures.com>
2022-02-28 14:45:00 -0500

    working with templat literals

--- js/shout.js
+++ js/shout.js
@@ -1,5 +1,5 @@
-const stringToShout = prompt("What do you want to shout");
-const shout = stringToShout.toLocaleUpperCase();
-const shoutMessage = `The message to shout is: ${shout}!!`;
+const stringToShout = prompt("What do you want to shout?");
+const shout = stringToShout.toUpperCase();
+const shoutMessage = `<h2>The message to shout is: ${shout}!!</h2>`;
 
 document.querySelector('main').innerHTML = shoutMessage;
--- js/story.js
+++ js/story.js
@@ -1,9 +1,15 @@
 // 1. Declare variables and capture input.
-const person = prompt("Give me a person");
-const place = prompt("Give me a place");
-const thing = prompt("Give me a thing");
+const adjective = prompt('Please type an adjective.');
+const verb = prompt('Please type a verb.');
+const noun = prompt('Please type a noun.');
+
 // 2. Combine the input with other words to create a story.
-const msg = `${person} went to ${place} to buy ${thing}`;
+const sentence = `<p>There once was a ${adjective} programmer who wanted to use JavaScript to ${verb} the ${noun}.</p>`;
 
 // 3. Display the story as a <p> inside the <main> element.
-document.querySelector('main').innerHTML = msg;
+document.querySelector('main').innerHTML = sentence;
+
+
+
+
+
